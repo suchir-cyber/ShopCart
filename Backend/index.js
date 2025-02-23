6d6334dd5b61c803b6c1e7c38b17872b43e48d80
suchir-cyber <suchirpandula@gmail.com>
2025-02-24 00:41:11 +0530

    create productController

--- Backend/index.js
+++ Backend/index.js
@@ -3,11 +3,12 @@ import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
-import userRoutes from './routes/userRoutes.js'
-//utils
-import connectDB from './config/db.js'
 
+//utils
+import userRoutes from './routes/userRoutes.js'
+import connectDB from './config/db.js'
 import categoryRoutes from './routes/categoryRoutes.js'
+import { productRoutes } from './routes/productRoutes.js'
 
 dotenv.config();
 const port = process.env.PORT || 5000;
@@ -22,5 +23,6 @@ app.use(cookieParser())
 
 app.use("/api/users",userRoutes);
 app.use("/api/category",categoryRoutes);
+app.use("/api/product",productRoutes);
 
 app.listen(port,() => console.log(`Server is working on port : ${port}`))
