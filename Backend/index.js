2ba84f3c5b30429256117ce09d0a38df3c5069e1
suchir-cyber <suchirpandula@gmail.com>
2025-02-23 07:30:42 +0530

    completed with categories backend

--- Backend/index.js
+++ Backend/index.js
@@ -7,6 +7,8 @@ import userRoutes from './routes/userRoutes.js'
 //utils
 import connectDB from './config/db.js'
 
+import categoryRoutes from './routes/categoryRoutes.js'
+
 dotenv.config();
 const port = process.env.PORT || 5000;
 
@@ -19,5 +21,6 @@ app.use(express.urlencoded({extended : true}))
 app.use(cookieParser())
 
 app.use("/api/users",userRoutes);
+app.use("/api/category",categoryRoutes);
 
 app.listen(port,() => console.log(`Server is working on port : ${port}`))
---
+++ Backend/routes/categoryRoutes.js
@@ -0,0 +1,22 @@
+import express from "express";
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
+
+import {
+    createCategory,
+    updateCategory,
+    removeCategory,
+    listCategory,
+    readCategory
+  } from "../controllers/categoryController.js";
+
+
+const router = express.Router();
+
+
+router.route("/").post(authenticate , authorizeAdmin , createCategory);
+router.route("/:categoryId").put(authenticate, authorizeAdmin, updateCategory);
+router.route("/:categoryId").delete(authenticate, authorizeAdmin, removeCategory);
+router.route("/categories").get(listCategory);
+router.route("/:id").get(readCategory);
+
+export default router;
