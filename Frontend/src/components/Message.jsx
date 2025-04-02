5ae8739f6fdabfe777d2cdf7c48693b8517a77d2
suchir-cyber <suchirpandula@gmail.com>
2025-04-02 15:19:17 +0530

    corrected one speeling mistake

--- Frontend/src/components/Message.jsx
+++ Frontend/src/components/Message.jsx
@@ -1,7 +1,7 @@
 const Message = ({ variant, children }) => {
     const getVariantClass = () => {
       switch (variant) {
-        case "succcess":
+        case "success":
           return "bg-green-100 text-green-800";
         case "error":
           return "bg-red-100 text-red-800";
